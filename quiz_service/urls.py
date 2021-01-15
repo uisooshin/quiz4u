0548449b148bb372e6f79894c2bcbc171c83f08f
uisooshin <uisooshin@gmail.com>
2021-01-15 22:19:19 +0900

    side bar modify, signin/signup button add

--- quiz_service/urls.py
+++ quiz_service/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),                    #root page -> index page
-    path('index.html', views.index, name='index'),          #index page
+    #path('index.html', views.index, name='index'),          #index page
+    path(r'index/', views.index, name='index'),          #index page
     path(r'solv_quiz/', views.solv_quiz, name='solv_quiz'), #quiz page
-    path(r'register/', views.register, name='register'),    #register page
+    path(r'signup/', views.signup, name='signup'),    #signup page
+    path(r'signin/', views.signin, name='signin'),    #signup page
+
 ]
--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,3 @@
-sudo python manage.py runserver &
+sudo python3 manage.py runserver 0.0.0.0:80 
+#sudo python manage.py runserver &
 #ngrok http 8000 > ngrok_result.log
